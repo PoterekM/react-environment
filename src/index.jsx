4a609773f2021fd963a383fbcf647906c396d05d
Michelle Poterek <poterekm@gmail.com>
2017-09-21 18:50:49 -0700

    install react and redux dependencies to connect to store

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-
+import { createStore } from 'redux';
+import reducer from './reducers/some-reducer';
+import { Provider } from 'react-redux'
 import App from "./components/App";
+
+const store = createStore(reducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
+    <Provider>
       <Component/>
-    </AppContainer>,
+  </Provider>,
     document.getElementById("react-app-root")
   );
 };
